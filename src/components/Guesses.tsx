915534e545542c22d145776af90c669c5724c2a6
ronensimanduyev <ronen.simanduyev1999@gmail.com>
2023-06-13 17:55:28 +0300

    added tries and applied logic

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -1,12 +1,23 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 type GuessesProps = {
     word: string[];
+    currentRow: number;
+    onAttempt: (attempt: string[]) => void;
 };
 
-const Guesses: React.FC<GuessesProps> = ({word}) => {
+const Guesses: React.FC<GuessesProps> = ({word, currentRow, onAttempt}) => {
     const [guess, setGuess] = useState<string[]>(['', '', '', '', '']);
+    const [previousAttempts, setPreviousAttempts] = useState<string[][]>([]);
     const inputRefs = useRef<HTMLInputElement[]>([]);
+    const formRef = useRef<HTMLFormElement>(null);
+
+    useEffect(() => {
+        if (formRef.current) {
+            formRef.current.reset();
+            inputRefs.current[0]?.focus();
+        }
+    }, [currentRow]);
 
     const handleChange = (index: number, value: string) => {
         const newGuess = [...guess];
@@ -18,55 +29,63 @@ const Guesses: React.FC<GuessesProps> = ({word}) => {
         }
     };
 
-    const handleKeyUp = (index: number, event: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyUp = (
+        index: number,
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
         if (event.key === 'Backspace' && index > 0 && guess[index].length === 0) {
             inputRefs.current[index - 1]?.focus();
         }
     };
 
-    function handleKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
-        if (event.key === "Enter") {
-            handleSubmit(event);
-        }
-    }
-
-
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
-        console.log(guess);
-        console.log(word);
-        // Perform any further processing or actions with the guess
 
-        // Clear the guess after submitting
+        if (guess.some((input) => input === '')) {
+            return <h1>You need to fill all the inputs.</h1>;
+        }
+
+        // console.log('guess:', guess.length, guess);
+        onAttempt(guess);
+        setPreviousAttempts((prevAttempts) => [...prevAttempts, guess]);
         setGuess(Array.from({length: word.length}, () => ''));
-    };
 
+        inputRefs.current[0]?.focus();
+    };
 
     return (
         <div>
-
-            <form onSubmit={handleSubmit} onKeyDown={() => handleKeyDown}>
-                <div className="flex items-center justify-center space-x-2">
-                    {guess.map((value, index) => (
-                        <input
-                            key={index}
-                            type="text"
-                            value={value}
-                            onChange={(e) => handleChange(index, e.target.value)}
-                            onKeyUp={(e) => handleKeyUp(index, e)}
-                            className="w-16 m-2 h-16 rounded-lg border text-black border-gray-300 text-2xl text-center"
-                            maxLength={1}
-                            ref={(ref) => {
-                                inputRefs.current[index] = ref!;
-                            }}
-                        />))}
+            {currentRow <= 6 && (
+                <form onSubmit={handleSubmit} ref={formRef}>
+                    <div className="flex items-center justify-center space-x-2">
+                        {guess.map((value, index) => (
+                            <input
+                                key={index}
+                                type="text"
+                                value={value}
+                                onChange={(e) => handleChange(index, e.target.value)}
+                                onKeyUp={(e) => handleKeyUp(index, e)}
+                                className="w-[65px] my-2 h-[65px] rounded-lg border text-black border-gray-300 text-2xl text-center"
+                                maxLength={1}
+                                ref={(ref) => {
+                                    inputRefs.current[index] = ref!;
+                                }}
+                            />
+                        ))}
+                    </div>
+                    <div
+                        className="flex items-center justify-center w-[93%] rounded-xl m-auto sm:w-[600px] h-10 bg-gray-500">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            )}
+            {currentRow > 1 && (
+                <div>
+                    {previousAttempts.map((attempt, index) => (
+                        <p key={index}>Previous Attempt {index + 1}: {attempt.join(' ')}</p>
+                    ))}
                 </div>
-                <div
-                    className="flex items-center justify-center w-[93%] rounded-xl m-auto sm:w-[600px] h-10 bg-gray-500">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-
+            )}
         </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import words from '../../data/words.json';
 import Guesses from '@/components/Guesses';
 
 export default function Home() {
     const [word, setWord] = useState<string[]>(['', '', '', '', '']);
     const [result, setResult] = useState('');
+    const [tries, setTries] = useState<string[][]>([]);
 
     useEffect(() => {
         const fetchWord = () => {
@@ -25,13 +26,39 @@ export default function Home() {
         console.log(word, ':word');
     }, [word]);
 
+    const handleAttempt = (attempt: string[]) => {
+        if (tries.length < 6) {
+            setTries((prevTries) => [...prevTries, attempt]);
+            console.log();
+            console.log(tries[tries.length - 1], 'tries');
+        }
+    };
+
     return (
         <>
             <h1 className="text-5xl text-center mt-[4vh] bg-gradient-to-r from-blue-500 to-green-500 text-transparent bg-clip-text font-semibold">
                 WORDLE
             </h1>
-            <Guesses word={word}/>
+            <div className="flex flex-col items-center space-y-2">
+                {tries.map((attempt: string[], index: number) => (
+                    <div key={index} className="flex space-x-2">
+                        {attempt.map((letter: string, letterIndex: number) => (
+                            <span
+                                key={letterIndex}
+                                className={`w-16 my-2 h-16 leading-[4rem] rounded-lg border text-black bg-white border-gray-300 text-2xl text-center 
+                                     ${word[letterIndex] === attempt[letterIndex] ? 'bg-green-500' :
+                                    `${word.includes(letter) ? 'bg-yellow-200' : ''}`}`}>
+                                {letter}
+                             </span>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <Guesses word={word} currentRow={tries.length + 1} onAttempt={handleAttempt}/>
+            {tries.length === 6 && <p>You have reached the maximum number of attempts.</p>}
             <p>{result}</p>
         </>
+
+
     );
 }
